00da8a4f5f178e975ace6c2cc1631bc7a4730539
songdi676 <670165552@qq.com>
2020-03-11 13:49:27 +0800

    修改npm配置

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "@/shape";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
