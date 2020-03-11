39f21282c0f8cdec1b00d495c876268d535c6eaf
songdi676 <670165552@qq.com>
2020-03-11 13:56:10 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Editor />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Editor from "@/components/Editor/index.vue";
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Editor
   }
 }
 </script>
